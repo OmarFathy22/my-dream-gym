d660b21e0fe82eaa266456580d7e0ace06024337
morata <morata@gmail.com>
2023-04-30 05:29:48 +0200

    new

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
    domains:['d205bpvrqc9yn1.cloudfront.net' , 'i.ytimg.com',"yt3.ggpht.com"]
     
